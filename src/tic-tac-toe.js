404bf22f5edd91395913e63989a707017a6e16f4
Tamar Bigvava <bigvava0205@gmail.com>
2021-08-30 12:38:47 +0400

    init:code

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,107 @@
 class TicTacToe {
-    constructor() {
+	constructor() {}
 
-    }
+	currentPlayer = "x";
+	gameField = [
+		[null, null, null],
+		[null, null, null],
+		[null, null, null],
+	];
 
-    getCurrentPlayerSymbol() {
+	getCurrentPlayerSymbol() {
+		return this.currentPlayer;
+	}
 
-    }
+	nextTurn(rowIndex, colIndex) {
+		if (this.gameField[rowIndex][colIndex] != null) {
+			return false;
+		}
+		this.gameField[rowIndex][colIndex] = this.currentPlayer;
+		if (this.currentPlayer === "o") {
+			this.currentPlayer = "x";
+		} else {
+			this.currentPlayer = "o";
+		}
+	}
 
-    nextTurn(rowIndex, columnIndex) {
+	isFinished() {
+		if (this.noMoreTurns() || this.getWinner() || this.isDraw()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-    }
+	getWinner() {
+		if (
+			this.gameField[0][0] === this.gameField[0][1] &&
+			this.gameField[0][1] === this.gameField[0][2]
+		) {
+			return this.gameField[0][0];
+		} else if (
+			this.gameField[1][0] === this.gameField[1][1] &&
+			this.gameField[1][1] === this.gameField[1][2]
+		) {
+			return this.gameField[1][0];
+		} else if (
+			this.gameField[2][0] === this.gameField[2][1] &&
+			this.gameField[2][1] === this.gameField[2][2]
+		) {
+			return this.gameField[2][0];
+		} else if (
+			this.gameField[0][0] === this.gameField[1][0] &&
+			this.gameField[1][0] === this.gameField[2][0]
+		) {
+			return this.gameField[0][0];
+		} else if (
+			this.gameField[0][1] === this.gameField[1][1] &&
+			this.gameField[1][1] === this.gameField[2][1]
+		) {
+			return this.gameField[0][1];
+		} else if (
+			this.gameField[0][2] === this.gameField[1][2] &&
+			this.gameField[1][2] === this.gameField[2][2]
+		) {
+			return this.gameField[0][2];
+		} else if (
+			this.gameField[0][0] === this.gameField[1][1] &&
+			this.gameField[1][1] === this.gameField[2][2]
+		) {
+			return this.gameField[0][0];
+		} else if (
+			this.gameField[2][0] === this.gameField[1][1] &&
+			this.gameField[1][1] === this.gameField[0][2]
+		) {
+			return this.gameField[2][0];
+		} else {
+			return null;
+		}
+	}
 
-    isFinished() {
+	noMoreTurns() {
+		let temp = [];
+		for (let i = 0; i < this.gameField.length; i++) {
+			this.gameField[i].forEach((cell) => {
+				if (cell) {
+					temp.push("a");
+				}
+			});
+		}
 
-    }
+		if (temp.length === 9) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-    getWinner() {
+	isDraw() {
+		return this.noMoreTurns() && !this.getWinner();
+	}
 
-    }
-
-    noMoreTurns() {
-
-    }
-
-    isDraw() {
-
-    }
-
-    getFieldValue(rowIndex, colIndex) {
-
-    }
+	getFieldValue(rowIndex, colIndex) {
+		return this.gameField[rowIndex][colIndex];
+	}
 }
 
 module.exports = TicTacToe;
